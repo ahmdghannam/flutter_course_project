c93283911aab7eab492401531ed810e779a72966
ahmdghannam <iia7mt@gmail.com>
2024-01-17 10:47:49 +0200

    enhanced the architecture of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'login.dart'; // Import the SignUpPage
-import 'firebase_options.dart';
+import 'view/login.dart'; // Import the SignUpPage
+import 'model/firebase/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
