fd8fcf39f0012415d58ec77f9a5773b1196beccf
samigogo <samigogo339@gmial.com>
2024-01-19 13:16:35 +0200

    Creating bottom navigation and fixing profile page

--- lib/view/HomePage.dart
+++ lib/view/HomePage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course_project/view/ProfilePage.dart';
 import 'package:flutter_course_project/view/TableCreatorPage.dart';
 import 'StartingPage.dart';
 import 'package:url_launcher/url_launcher.dart';
+
 
 _launchURLInBrowser(String url) async {
   if (await canLaunch(url)) {
@@ -10,6 +12,8 @@ _launchURLInBrowser(String url) async {
     print('Could not launch $url');
   }
 }
+
+final List<Widget> pages = [HomePage(), StartingPage(), ProfilePage()];
 
 class HomePage extends StatelessWidget {
   @override
@@ -90,13 +94,14 @@ class HomePage extends StatelessWidget {
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 0,
+        type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.table_chart),
+            icon: Icon(Icons.app_registration),
             label: 'Create New Table',
           ),
           BottomNavigationBarItem(
@@ -104,6 +109,12 @@ class HomePage extends StatelessWidget {
             label: 'Profile',
           ),
         ],
+        onTap: (index){
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => pages[index]),
+          );
+        },
       ),
     );
   }
--- lib/view/ProfilePage.dart
+++ lib/view/ProfilePage.dart
@@ -1,29 +1,9 @@
 import 'package:flutter/material.dart';
+import 'HomePage.dart';
+import 'StartingPage.dart';
 
 void main() {
-  runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Icon(
-                Icons.logout,
-                color: Colors.red,
-              ),
-              Container(width: 8),
-              const Text(
-                "logout",
-                style: TextStyle(fontSize: 16, color: Colors.red),
-              ),
-            ],
-          ),
-          backgroundColor: Colors.white,
-        ),
-        body: const ProfilePage(),
-      )));
+  runApp(ProfilePage());
 }
 
 class ProfilePage extends StatefulWidget {
@@ -33,46 +13,101 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
+final List<Widget> pages = [HomePage(), StartingPage(), ProfilePage()];
+
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: EdgeInsets.only(bottom: 32),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          VerticalSpacing(36),
-          Center(child: Image.asset("assets/profilePlaceHolder.png")),
-          VerticalSpacing(16),
-          Center(child: Text("John Doe",style: TextStyle(fontSize: 22),)),
-          VerticalSpacing(24),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 60),
-            child: Divider(
-              height: 1,
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(
+                  Icons.logout,
+                  color: Colors.red,
+                ),
+                Container(width: 8),
+                const Text(
+                  "logout",
+                  style: TextStyle(fontSize: 16, color: Colors.red),
+                ),
+              ],
+            ),
+            backgroundColor: Colors.white,
+          ),
+          body: SingleChildScrollView(
+            padding: EdgeInsets.only(bottom: 32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                VerticalSpacing(36),
+                Center(child: Image.asset("assets/profilePlaceHolder.png")),
+                VerticalSpacing(16),
+                Center(
+                    child: Text(
+                  "John Doe",
+                  style: TextStyle(fontSize: 22),
+                )),
+                VerticalSpacing(24),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 60),
+                  child: Divider(
+                    height: 1,
+                  ),
+                ),
+                VerticalSpacing(44),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("email  : john@gmail.com"),
+                      Icon(Icons.edit)
+                    ],
+                  ),
+                ),
+                VerticalSpacing(8),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Text("id : 1-570-236-7033"),
+                )
+              ],
             ),
           ),
-          VerticalSpacing(44),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [Text("email  : john@gmail.com"), Icon(Icons.edit)],
-            ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: 0,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.app_registration),
+                label: 'Create New Table',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+                label: 'Profile',
+              ),
+            ],
+            onTap: (index) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => pages[index]),
+              );
+            },
           ),
-          VerticalSpacing(8),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            child: Text("id : 1-570-236-7033"),
-          )
-        ],
-      ),
-    );
+        ));
   }
+}
 
-  Container VerticalSpacing(double value) {
-    return Container(
-      height: value,
-    );
-  }
+Container VerticalSpacing(double value) {
+  return Container(
+    height: value,
+  );
 }
